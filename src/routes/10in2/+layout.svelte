7e91383fc1b664289f67dc0f5c3b3c41c7042403
harry <derharry@users.noreply.github.com>
2024-02-27 18:30:57 +0100

    init bkp

---
+++ src/routes/10in2/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    //import '@picocss/pico'
+    //import '../app.css'
+</script>
+
+<h1>10in1</h1>
+
+
+<nav>
+    sadfsaflkj
+</nav>
+
+<main>
+    <slot />
+</main>
---
+++ src/routes/zeepress/+layout.svelte
@@ -0,0 +1,19 @@
+<h1>ZeePress / Blogs</h1>
+
+<main>
+
+    <aside>
+        <input type="search" />
+
+        <ul>
+            <li>links naar blogs</li>
+        </ul>
+    </aside>
+
+    <article>
+        <slot>
+            Select an article
+        </slot>
+    </article>
+
+</main>
