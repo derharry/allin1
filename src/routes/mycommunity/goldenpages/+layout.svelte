afdf8dbb2a3a2c3d97e3c3d46ccae787f6c67cbe
derharry <derharry@users.noreply.github.com>
2024-05-15 13:33:51 +0200

    save added gouden gids

--- src/routes/mycommunity/goldenpages/+layout.svelte
+++ src/routes/mycommunity/goldenpages/+layout.svelte
@@ -23,26 +23,7 @@
 
 
     <div class="footer">
-        <p>&nbsp;</p>
-        <footer style="background-color: beige">
-            <table width="100%">
-                <tr>
-                    <td width="50%">
-                        <div class="left">
-                            * Disclaimer *<br>
-                            Deze Gouden Gids ..                    
-                            
-                        </div>
-                    </td>
-                    <td>
-                        <div class="right" style="">
-                            <p>Uw bedrijf staat er niet bij?<br>
-                                <a href="/mycommunity/goldenpages/register">Jouw bedrijf registreeren</a></p>
-                        </div>
-                    </td>
-                </tr>
-            </table>
-        </footer>
+
     </div>
         
 
